5b4ea264c29ee9cca0cfc797956ad8b37844c8c0
iman seif <mhseif90@gmail.com>
2020-12-21 19:14:29 +0330

    v 1.0.2

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>وب پکس WEB PA</title>
+    <title>وب پکس WEB PAxx</title>
 
 
     <link rel="stylesheet" href="{{asset('css/normalize.css')}} ">
